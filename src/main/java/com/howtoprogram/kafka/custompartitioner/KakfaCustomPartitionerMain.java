9918ac2667967661b07de286ee4a515e980b9da6
Ankit Kumar <32848378+ankit0209@users.noreply.github.com>
2018-11-16 12:38:32 +0530

    Initial commit

---
+++ src/main/java/com/howtoprogram/kafka/custompartitioner/KakfaCustomPartitionerMain.java
@@ -0,0 +1,37 @@
+package com.howtoprogram.kafka.custompartitioner;
+
+public final class KakfaCustomPartitionerMain {
+
+  public static void main(String[] args) {
+
+    String brokers = "localhost:9092";
+    String groupId = "group01";
+    String topic = "ankit2";
+
+    if (args != null && args.length == 3) {
+      brokers = args[0];
+      groupId = args[1];
+      topic = args[2];
+    }
+
+    // Start User Producer Thread
+    UserProducerThread producerThread = new UserProducerThread(brokers, topic);
+    Thread t1 = new Thread(producerThread);
+    t1.start();
+    try {
+        Thread.sleep(2000);
+      } catch (InterruptedException ie) {
+
+      }
+    // Start group of User Consumer Thread
+    UserConsumerThread consumerThread = new UserConsumerThread(brokers, groupId, topic);
+    Thread t2 = new Thread(consumerThread);
+    t2.start();
+
+    try {
+      Thread.sleep(10000);
+    } catch (InterruptedException ie) {
+
+    }
+  }
+}
